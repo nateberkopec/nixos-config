a59b18ef1207d628aa23caf53c0cb991a9e1f1a8
Nate Berkopec <nate.berkopec@gmail.com>
2023-02-16 21:46:35 -0800

    funcsave quiet, i3 windows

--- users/nateberkopec/config.fish
+++ users/nateberkopec/config.fish
@@ -61,7 +61,7 @@ mkdir -p $HOME/.vim/{backup,swap,undo}
 # Do not show any greeting
 set --universal --erase fish_greeting
 function fish_greeting; end
-funcsave fish_greeting
+funcsave -q fish_greeting
 
 # bobthefish theme
 set -g theme_color_scheme dracula
